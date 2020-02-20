bf2f94a9b1cf7d9d65a510a64aafbb55d95ea052
mrlee24 <lymanhlong@gmail.com>
2020-02-19 21:35:52 -0500

    TestAgainAgain

---
+++ Sekiro/Assets/Script/Character/CharacterStat.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public abstract class CharacterStat : MonoBehaviour
+{
+    private Stat _damage;
+    private HealthSystem _healthSystem;
+    private PostureSystem _postureSystem;
+    public bool isAlive = true;
+
+    public Stat Damage { get => _damage; set => _damage = value; }
+    public HealthSystem HealthSystem { get => _healthSystem; set => _healthSystem = value; }
+    public PostureSystem PostureSystem
+    {
+        get => _postureSystem; set => _postureSystem = value;
+    }
+}
---
+++ Sekiro/Assets/Script/Character/PlayerStat.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStat : CharacterStat
+{
+    private Vector3 lastPosition;
+    //private Transform currentPosition = Transform.fi;
+
+    //public Vector3 PlayerPosition()
+    //{
+
+    //}
+    public Vector3 LastPlayerPosition()
+        => lastPosition;
+}
